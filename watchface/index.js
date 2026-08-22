3286b20439545ccc61939ec15f9bc6b6d36d0976
TestAF Dev <dev@testaf.local>
2026-08-22 10:26:19 +0500

    Init balance2 basic watchface v1.0.0: time HH:MM + canvas separator line

---
+++ watchface/index.js
@@ -0,0 +1,59 @@
+import { align, createWidget, prop, text_style, widget } from '@zos/ui'
+import { Time } from '@zos/sensor'
+
+const SCREEN_SIZE = 480
+const TIME_HEIGHT = 120
+
+// Decorative line under the time digits
+const LINE_WIDTH = 200
+const LINE_HEIGHT = 6
+const LINE_Y = 300 // vertical center of the line (tweak to taste)
+
+function twoDigits(value) {
+  return value < 10 ? `0${value}` : `${value}`
+}
+
+WatchFace({
+  build() {
+    const time = new Time()
+
+    // Canvas layer (bottom) for custom drawing (ZeppOS API 3.0 canvas)
+    const canvas = createWidget(widget.CANVAS, {
+      x: 0,
+      y: 0,
+      w: SCREEN_SIZE,
+      h: SCREEN_SIZE,
+    })
+    canvas.setPaint({ color: 0xffffff, line_width: LINE_HEIGHT })
+    canvas.drawLine({
+      x1: (SCREEN_SIZE - LINE_WIDTH) / 2,
+      y1: LINE_Y,
+      x2: (SCREEN_SIZE + LINE_WIDTH) / 2,
+      y2: LINE_Y,
+      color: 0xffffff,
+    })
+
+    const timeWidget = createWidget(widget.TEXT, {
+      x: 0,
+      y: (SCREEN_SIZE - TIME_HEIGHT) / 2,
+      w: SCREEN_SIZE,
+      h: TIME_HEIGHT,
+      color: 0xffffff,
+      text_size: 96,
+      align_h: align.CENTER_H,
+      align_v: align.CENTER_V,
+      text_style: text_style.NONE,
+      text: '00:00',
+    })
+
+    const updateTime = () => {
+      const value = `${twoDigits(time.getHours())}:${twoDigits(time.getMinutes())}`
+      timeWidget.setProperty(prop.MORE, { text: value })
+      console.log(`[balance2-basic] time updated: ${value}`)
+    }
+
+    updateTime()
+    time.onPerMinute(updateTime)
+    console.log('[balance2-basic] watch face started')
+  },
+})
